28df09d7c0c3a1bb479405aef6d29813ed6b8bf3
Daniel Büttner <daniel.buettner@posteo.de>
2023-04-17 15:00:52 +0200

    upload solution to Versuch 1

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -11,7 +11,25 @@ public class Receive_Mail {
 	
 	public static void fetchMail() {
 		try {
-			// your code here
+			Properties props = System.getProperties();
+			props.put("mail.pop3.host", "localhost");
+			Session session = Session.getInstance(props);
+
+			Store store = session.getStore("pop3");
+			store.connect("localhost", "labrat", "kn1lab");
+
+			Folder folder = store.getFolder("INBOX");
+			folder.open(Folder.READ_ONLY);
+
+			Message[] messages = folder.getMessages();
+
+			for (int i = 0; i < messages.length; i++) {
+				System.out.println(messages[i].getFrom()[0]);
+				System.out.println(messages[i].getSentDate());
+				System.out.println(messages[i].getSubject());
+				System.out.println(messages[i].getMessageNumber());
+				System.out.println(messages[i].getContent().toString());
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- versuch1/src/Send_Mail.java
+++ versuch1/src/Send_Mail.java
@@ -1,9 +1,6 @@
+import java.util.Date;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage; 
 
@@ -14,7 +11,22 @@ public class Send_Mail {
 	
 	public static void sendMail() {
 		try {
-			// your code here
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "localhost");
+			Session session = Session.getInstance(props);
+
+			try {
+				MimeMessage msg = new MimeMessage(session);
+				msg.setFrom("labrat@localhost");
+				msg.setRecipients(Message.RecipientType.TO,
+						"labrat@localhost");
+				msg.setSubject("JavaMail Test Mail");
+				msg.setSentDate(new Date());
+				msg.setText("Hello, world!\n");
+				Transport.send(msg);
+			} catch (MessagingException mex) {
+				System.out.println("send failed, exception: " + mex);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
